31715ff2a3e413c7b69e25e0f73b0f3ae78db77b
David Vavra <me@destil.cz>
2011-05-07 21:52:09 +0200

    redesigned lock screen

--- src/cz/destil/fixbrokenpb/LockScreen.java
+++ src/cz/destil/fixbrokenpb/LockScreen.java
@@ -20,78 +20,38 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 package cz.destil.fixbrokenpb;
 
 import java.text.DateFormat;
-import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 
 import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.SystemClock;
 import android.telephony.TelephonyManager;
 import android.view.View;
 import android.widget.TextView;
 
 
 public class LockScreen extends Activity implements View.OnClickListener {
-    private DateFormat dateFormatter = DateFormat.getDateInstance();
-    private DateFormat timeFormatter = DateFormat.getTimeInstance();
-    private DateFormat dayOfWeekFormatter = new SimpleDateFormat("E / F");
+    private DateFormat timeFormatter = DateFormat.getTimeInstance(DateFormat.SHORT);
     private TelephonyManager tm;
     private TextView timeView;
-    private TextView dateView;
-    private TextView dowView;
-    private TextView batteryView;
-    private Handler mHandler;
-    private long mStartTime;
-
-    private static String TAG = "LockScreen";
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.lock_screen);
         tm = (TelephonyManager)getSystemService(Context.TELEPHONY_SERVICE);
-        dateView = (TextView)findViewById(R.id.date);
         timeView = (TextView)findViewById(R.id.time);
-        timeView = (TextView)findViewById(R.id.time);
-        dowView = (TextView)findViewById(R.id.dow);
-        batteryView = (TextView)findViewById(R.id.battery);
-        mHandler = new Handler();
 
     }
     @Override
     public void onResume(){
         Date nd = Calendar.getInstance().getTime();
-        dateView.setText(dateFormatter.format(nd));
         timeView.setText(timeFormatter.format(nd));
-        dowView.setText(dayOfWeekFormatter.format(nd));
         if(tm.getCallState() != 0){
             finish();
         }
-        mStartTime = System.currentTimeMillis();
-        mHandler.removeCallbacks(mUpdateTimeTask);
-        mHandler.postDelayed(mUpdateTimeTask, 100);
-
         super.onResume();
     }
-    @Override
-    public void onPause(){
-        mHandler.removeCallbacks(mUpdateTimeTask);
-        super.onPause();
-    }
-
-    private Runnable mUpdateTimeTask = new Runnable() {
-        public void run() {
-            long start = SystemClock.uptimeMillis();
-
-            Date nd = Calendar.getInstance().getTime();
-            timeView.setText(timeFormatter.format(nd));
-
-
-            mHandler.postDelayed(this, 1000);
-        }
-    };
     @Override
     public void onClick(View v) {}
 }
